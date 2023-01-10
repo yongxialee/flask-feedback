29cbb1fe1135a9edd3969eb3fe67756b6fddf91b
yongxialee leelianou <dejnaj@hotmail.com>
2023-01-10 15:53:22 -0700

    fix app.pu file

--- app.py
+++ app.py
@@ -127,13 +127,14 @@ def add_feedback(username):
         return render_template("feedback/add_feedback.html", form=form)
 
 # GET & POST /users/<username>/feedback-id/update
-@app.route('/users/feedback/<int:feedback_id>/update',methods=["GET","POST"])
+@app.route('/feedback/<int:feedback_id>/update',methods=["GET","POST"])
 def update_feedback(feedback_id):
     """show update feedback form and process it"""
+    feedback = Feedback.query.get_or_404(feedback_id)
     if "username" not in session or feedback.username != session['username']:
         raise Unauthorized()
     
-    feedback = Feedback.query.get_or_404(feedback_id)
+    
     form = FeedbackForm(obj=feedback)
 
     if form.validate_on_submit():
@@ -146,17 +147,19 @@ def update_feedback(feedback_id):
 
     return render_template("feedback/edit.html", form=form, feedback=feedback)
 
-@app.route('/users/feedback/<int:feedback_id>/delete', methods=["POST"])
+@app.route('/feedback/<int:feedback_id>/delete', methods=["POST"])
 def delete_feedback(feedback_id):
     """delete a feedback"""
+    feedback = Feedback.query.get(feedback_id)
     if "username" not in session or feedback.username != session['username']:
         raise Unauthorized()
-    feedback = Feedback.query.get(feedback_id)
+    
     
     form = RemoveForm()
-    
+    print("*************" ,feedback.username)
     if form.validate_on_submit():
         db.session.delete(feedback)
         db.session.commit()
-        return redirect(f"/users/{feedback.username}")
+        
+    return redirect(f"/users/{feedback.username}")
       
